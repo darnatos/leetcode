d2c3be1c4313874dbb2654a2b1001c8d83e52158
ping <eremes1016@gmail.com>
2021-08-16 22:44:05 +0800

    feat: min sub array len

---
+++ solution/209-minimun-size-subarray-sum.go
@@ -0,0 +1,32 @@
+package solution
+
+import (
+	"leetcode/util"
+	"math"
+)
+
+func MinSubArrayLen(target int, nums []int) int {
+	l, sum := 0, 0
+	res := math.MaxInt32
+	n := len(nums)
+
+	for i := 0; i < n; i++ {
+		sum += nums[i]
+
+		if sum >= target {
+			res = util.Min(res, i-l+2)
+
+			for sum >= target {
+				sum -= nums[l]
+				l++
+
+				res = util.Min(res, i-l+2)
+			}
+		}
+	}
+
+	if res == math.MaxInt32 {
+		return 0
+	}
+	return res
+}

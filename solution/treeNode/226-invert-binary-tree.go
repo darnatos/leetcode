570eb74ab599d5e67007fbfb029f6d22a3d66f8a
ping <eremes1016@gmail.com>
2021-08-22 15:07:38 +0800

    feat: invert binary tree

---
+++ solution/treeNode/226-invert-binary-tree.go
@@ -0,0 +1,13 @@
+package treeNode
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	tmp := root.Left
+	root.Left = root.Right
+	root.Right = tmp
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}

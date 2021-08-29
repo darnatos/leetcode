2ac01530c59c2e5ce1c7845bdf00439912a0cae5
eremes <eremes1016@gmail.com>
2021-08-30 01:05:08 +0900

    feat: rotate image

---
+++ solution/48-rotate-image.go
@@ -0,0 +1,13 @@
+package solution
+
+func rotate(matrix [][]int)  {
+	for i, j := 0, len(matrix)-1; i < j; i, j = i+1, j-1 {
+		matrix[i], matrix[j] = matrix[j], matrix[i]
+	}
+
+	for i := 0; i < len(matrix)-1; i++ {
+		for j := i + 1; j < len(matrix); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}

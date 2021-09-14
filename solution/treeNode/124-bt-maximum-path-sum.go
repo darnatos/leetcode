48d77998a620e0dddf480a4d1329ca0291ffe7ab
eremes <eremes1016@gmail.com>
2021-09-14 23:09:10 +0900

    feat: binary tree max path sum

---
+++ solution/treeNode/124-bt-maximum-path-sum.go
@@ -0,0 +1,33 @@
+package treeNode
+
+import (
+	"leetcode/util"
+	"math"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxPathSum(root *TreeNode) int {
+	res := math.MinInt32
+	mpsHelper(root, &res)
+	return res
+}
+
+func mpsHelper(root *TreeNode, res *int) int {
+	if root == nil {
+		return 0
+	}
+
+	l := util.Max(mpsHelper(root.Left, res), 0)
+	r := util.Max(mpsHelper(root.Right, res), 0)
+	m := root.Val
+
+	*res = util.Max(*res, l+m+r)
+	return util.Max(l, r)+m
+}

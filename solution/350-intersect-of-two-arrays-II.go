ad2085f5be49bb605edfdab49082c891b04a0998
eremes <eremes1016@gmail.com>
2021-09-17 21:01:09 +0900

    feat: intersect of 2 arrays

---
+++ solution/350-intersect-of-two-arrays-II.go
@@ -0,0 +1,24 @@
+package solution
+
+import "sort"
+
+func intersect(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	res := make([]int, 0, 4)
+	i, j := 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] == nums2[j] {
+			res = append(res, nums1[i])
+			i++
+			j++
+		} else if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
+		}
+	}
+
+	return res
+}

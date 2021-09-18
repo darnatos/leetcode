661d211a697bd2206ce108adcdf8b7e04d69ea7d
ping <eremes1016@gmail.com>
2021-09-18 08:47:43 +0800

    feat: reverse linked list

---
+++ solution/myLinkedList/206-reverse-linked-list.go
@@ -0,0 +1,14 @@
+package myLinkedList
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+
+	return prev
+}

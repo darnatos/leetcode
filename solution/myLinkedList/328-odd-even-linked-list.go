08d31cf77e4f9b7b7b238bf50cceb2f002418059
ping <eremes1016@gmail.com>
2021-12-02 21:23:09 +0800

    feat: odd even list

---
+++ solution/myLinkedList/328-odd-even-linked-list.go
@@ -0,0 +1,26 @@
+package myLinkedList
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	odd, even := head, head.Next
+	ehead := even
+	for even != nil && even.Next != nil {
+		odd.Next = odd.Next.Next
+		even.Next = even.Next.Next
+		odd = odd.Next
+		even = even.Next
+	}
+	odd.Next = ehead
+
+	return head
+}

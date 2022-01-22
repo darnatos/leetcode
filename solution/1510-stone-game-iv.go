c11d9875437232e236523973db33a9c7ee3200ff
ping <eremes1016@gmail.com>
2022-01-23 00:28:00 +0800

    feat: stone game iv

---
+++ solution/1510-stone-game-iv.go
@@ -0,0 +1,15 @@
+package solution
+
+func winnerSquareGame(n int) bool {
+	dp := make([]bool, n+1)
+	dp[1] = true
+	for i := 1; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			if !dp[i-j*j] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}

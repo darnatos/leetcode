979835511c677b528b622bf24fb4bb37b1c52913
eremes <eremes1016@gmail.com>
2022-06-18 11:59:55 +0900

    feat: delet operation for two strings

---
+++ solution/583-delete-operation-for-two-strings.go
@@ -0,0 +1,19 @@
+package solution
+
+func minDistance2(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if word1[i] == word2[j] {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return m + n - 2*dp[m][n]
+}
